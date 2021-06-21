ca8635163fb7695e3f50300d115d7a95596ee459
Mecaneer23 <74385377+Mecaneer23@users.noreply.github.com>
2021-06-21 00:42:42 -0500

    Add files via upload

---
+++ compiler.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+# takes a file that is completely ones and zeros - any non-valid character is treated as a comment
+# outputs python output
+
+import sys, os
+
+if sys.argv[1][-3:] == ".bs":
+	with open(sys.argv[1]) as f:
+		temp = ""
+		counter = 0
+		chars = []
+		output = ""
+		for i in f.read():
+			if i in ["0", "1"]:
+				temp += i
+				counter += 1
+				if counter == 8:
+					chars.append(temp)
+					temp = ""
+					counter = 0
+		for i in chars:
+			output += chr(int(i, 2))
+		with open(f"{sys.argv[1][:-3]}.py", 'w') as g:
+			g.write(output)
+		print(f"Created: {sys.argv[1][:-3]}.py")
+else:
+	print("This isn't bs!")
+	exit()
