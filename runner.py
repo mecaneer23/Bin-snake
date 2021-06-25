b561f5e292e94d65aa08f02271943ea7d04e4f28
Mecaneer23 <74385377+Mecaneer23@users.noreply.github.com>
2021-06-24 19:22:41 -0500

    Add files via upload

--- runner.py
+++ runner.py
@@ -5,23 +5,36 @@
 
 import sys, os
 
-if sys.argv[1][-3:] == ".bs":
-	with open(sys.argv[1]) as f:
-		temp = ""
-		counter = 0
-		chars = []
-		output = ""
-		for i in f.read():
-			if i in ["0", "1"]:
-				temp += i
-				counter += 1
-				if counter == 8:
-					chars.append(temp)
-					temp = ""
-					counter = 0
-		for i in chars:
-			output += chr(int(i, 2))
-		print(str(exec(output))[:-4], end="")
-else:
-	print("This isn't bs!")
-	exit()
+try:
+	if sys.argv[1][-3:] == ".bs":
+		with open(sys.argv[1]) as f:
+			temp = ""
+			counter = 0
+			chars = []
+			output = ""
+			for i in f.read():
+				if i in ["0", "1"]:
+					temp += i
+					counter += 1
+					if counter == 8:
+						chars.append(temp)
+						temp = ""
+						counter = 0
+			for i in chars:
+				output += chr(int(i, 2))
+			print(str(exec(output))[:-4], end="")
+	else:
+		print("This isn't bs!")
+		exit()
+except IndexError:
+	print("Make sure to include a Bin-snake file, for example: ", end="")
+	if sys.argv[0] == "runner.py":
+		if sys.platform == "win32":
+			print(f"python runner.py filename.bs")
+		else:
+			print(f"python3 runner.py filename.bs")
+	elif sys.argv[0] == "runner.exe":
+		print(f"runner.exe filename.bs")
+	else:
+		print("Failed to run")
+		
