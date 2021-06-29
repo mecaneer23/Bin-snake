c3b0910a44ed0ff63efb9b0017e713004c9e5116
Mecaneer23 <74385377+Mecaneer23@users.noreply.github.com>
2021-06-28 19:53:19 -0500

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,31 +1,33 @@
 #!/bin/python3
 
-# takes a file that is completely ones and zeros - any non-valid character is treated as a comment
-# outputs python output
+import sys
 
-import sys, os
-
+def logic(text):
+	temp = ""
+	counter = 0
+	chars = []
+	output = ""
+	for i in text:
+		if i in ["0", "1"]:
+			temp += i
+			counter += 1
+			if counter == 8:
+				chars.append(temp)
+				temp = ""
+				counter = 0
+	for i in chars:
+		output += chr(int(i, 2))
+	print(str(exec(output))[:-4], end="")
 try:
 	if sys.argv[1][-3:] == ".bs":
 		with open(sys.argv[1]) as f:
-			temp = ""
-			counter = 0
-			chars = []
-			output = ""
-			for i in f.read():
-				if i in ["0", "1"]:
-					temp += i
-					counter += 1
-					if counter == 8:
-						chars.append(temp)
-						temp = ""
-						counter = 0
-			for i in chars:
-				output += chr(int(i, 2))
-			print(str(exec(output))[:-4], end="")
+			logic(f.read())
 	else:
-		print("This isn't bs!")
-		exit()
+		for i in sys.argv[1]:
+			if i not in ["0", "1"]:
+				print("This isn't bs!")
+				exit()
+		logic(sys.argv[1])
 except IndexError:
 	print("Make sure to include a Bin-snake file, for example: ", end="")
 	if sys.argv[0] == "runner.py":
@@ -37,4 +39,3 @@ except IndexError:
 		print(f"runner.exe filename.bs")
 	else:
 		print("Failed to run")
-		
