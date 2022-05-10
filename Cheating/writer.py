fd557dba1bf08a03eb884e78af4f596a1fdcbaa1
mecaneer23 <mecaneer23@gmail.com>
2022-05-10 12:26:22 -0500

    modify writer

--- Cheating/writer.py
+++ Cheating/writer.py
@@ -1,27 +1,20 @@
 #!/bin/python3
 
-# takes a python file - outputs bin-snake file (.bs)
-
 import sys
 
 def add_zero(binary):
 	if len(binary) < 8:
-		binary = f"0{binary}"
-		binary = add_zero(binary)
-	if len(binary) == 8:
-		return binary
+		return add_zero(f"0{binary}")
+	return binary
 
-if sys.argv[1][-3:] == ".py":
-	pass
-else:
+if len(sys.argv) <= 1:
+	print("Error: No file specified!")
+	exit()
+if sys.argv[1][-3:] != ".py":
 	print("This isn't a python file!")
 	exit()
 
-with open(sys.argv[1]) as f:
-	output = ""
-	for i in f.read():
-		binary = bin(ord(i))[2:]
-		output += add_zero(binary)
-with open(f"{sys.argv[1][:-3]}.bs", 'w') as g:
-	g.write(output)
+with open(sys.argv[1], 'r') as f:
+	with open(f"{sys.argv[1][:-3]}.bs", 'w') as g:
+		g.write("".join(add_zero(bin(ord(i))[2:]) for i in f.read()))
 print(f"Created: {sys.argv[1][:-3]}.bs")
